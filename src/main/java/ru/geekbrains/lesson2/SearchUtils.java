f4e1b1e6a15ef37bfe67eecc7dd29b34fa9fa6b6
Mariya <impa1002@gmail.com>
2023-07-26 15:09:08 +0300

    добавлен файл lesson2

---
+++ src/main/java/ru/geekbrains/lesson2/SearchUtils.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.lesson2;
+
+public class SearchUtils {
+
+
+    public static int binarySearch(int[] array, int value){
+        return binarySearch(array, value, 0, array.length - 1);
+    }
+    private static int binarySearch(int[] array, int value, int left, int right){
+
+        if (right < left)
+            return -1;
+
+        int middle = (right - left) / 2 + left;
+        if (array[middle] == value){
+            return middle;
+        }
+        else if (array[middle] < value){
+            return binarySearch(array, value, middle + 1, right);
+        }
+        else {
+            return binarySearch(array, value, left, middle - 1);
+        }
+
+    }
+
+}
