65f9117f3fc7e11e45bc0f52c9f691309fbdeaf1
Mariya <impa1002@gmail.com>
2023-08-05 11:37:54 +0300

    добавлена папка lesson2

--- src/main/java/ru/geekbrains/lesson2/EX_1.java
+++ src/main/java/ru/geekbrains/lesson2/EX_1.java
@@ -1,12 +1,26 @@
 package ru.geekbrains.lesson2;
 
-import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class Program {
+public class EX_1 {
 
     public static void main(String[] args) {
+        float num = 0;
+        boolean flag = true;
+        do{
+            try{
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите число (дробное число вводится через запятую): ");
+                num = in.nextFloat();
+                flag = false;
+            }catch(InputMismatchException e){
+                System.out.println("Введённое значение не является числом, поробуйте снова.");
+            }
+        }while(flag);
+        System.out.println(num);
 
-        
+
     }
 
 }
